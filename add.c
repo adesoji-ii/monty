38be28de8826d024b10218fa11f21395126602a8
Oluwapelumi Ojo <67923042+adesoji-ii@users.noreply.github.com>
2023-12-18 22:14:31 +0100

    Making changes

--- add.c
+++ add.c
@@ -5,23 +5,30 @@
  * @head: stack head
  * @counter: line_number
  * Return: no return
- */
+*/
 
 void f_add(stack_t **head, unsigned int counter)
 {
-	/* Check if stack has less than two elements */
-	if (*head == NULL || (*head)->next == NULL)
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
-		handle_error();
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
-
-	int result = (*head)->n + (*head)->next->n;
-	(*head)->next->n = result;
-
-	/* Remove the top element */
-	stack_t *temp = *head;
-	*head = (*head)->next;
-	(*head)->prev = NULL;
-	free(temp);
+	h = *head;
+	aux = h->n + h->next->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
 }
