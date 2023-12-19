8a406b076c3a92fe0f0c6e606f2f24b4feace4b8
Oluwapelumi Ojo <67923042+adesoji-ii@users.noreply.github.com>
2023-12-19 18:30:40 +0100

    Update

--- queue.c
+++ queue.c
@@ -1,39 +1,50 @@
 #include "monty.h"
 
 /**
- * addqueue - adds a node to the end of the stack (queue behavior)
- * @head: pointer to the head of the stack
- * @n: new value to be added
+ * f_queue - a function that prints the top
+ * @head: stack head
+ * @counter: line_number
  * Return: no return
- */
+*/
 
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - add node to the tail stack
+ * @n: new_value
+ * @head: head of the stack
+ * Return: no return
+*/
 void addqueue(stack_t **head, int n)
 {
 	stack_t *new_node, *aux;
 
+	aux = *head;
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
-		fprintf(stderr, "Memory allocation failed\n");
-		handle_error();
+		printf("Error\n");
 	}
-
 	new_node->n = n;
 	new_node->next = NULL;
-
-	if (*head == NULL)
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
 	{
 		*head = new_node;
 		new_node->prev = NULL;
-		return;
 	}
-
-	aux = *head;
-	while (aux->next != NULL)
+	else
 	{
-		aux = aux->next;
+		aux->next = new_node;
+		new_node->prev = aux;
 	}
-
-	aux->next = new_node;
-	new_node->prev = aux;
 }
