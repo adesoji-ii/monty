b1f7c2ecb917fef88340acd9d589f444cc636125
Oluwapelumi Ojo <67923042+adesoji-ii@users.noreply.github.com>
2023-12-19 18:46:51 +0100

    Update

--- pchar.c
+++ pchar.c
@@ -1,43 +1,33 @@
 #include "monty.h"
-#include <ctype.h>
 
 /**
- * f_pchar - prints the char at the top of the stack,
- * followed by a new line
+ * f_pchar - a function that prints the char at the top
+ * of the stack, followed by a new line
  * @head: stack head
  * @counter: line_number
  * Return: no return
- */
+*/
 
 void f_pchar(stack_t **head, unsigned int counter)
 {
-	stack_t *h = *head;
+	stack_t *h;
 
+	h = *head;
 	if (!h)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
-		handle_error();
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
-
-	if (!isprint(h->n))
+	if (h->n > 127 || h->n < 0)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
-		handle_error();
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
-
 	printf("%c\n", h->n);
-}
-
-/* Error handling function */
-
-/**
- * handle_error - handles error conditions and exits the program.
- */
-
-void handle_error(void)
-{
-	fclose(bus.file);
-	free(bus.content);
-	free_stack(bus.head);
-	exit(EXIT_FAILURE);
 }
