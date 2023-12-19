1292a778ddae61a48a53a73988571db9b1367211
Oluwapelumi Ojo <67923042+adesoji-ii@users.noreply.github.com>
2023-12-19 18:37:46 +0100

    Update

--- sub.c
+++ sub.c
@@ -2,26 +2,30 @@
 
 /**
  * f_sub - subtracts the top element from the second-top element of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
  */
 
 void f_sub(stack_t **head, unsigned int counter)
 {
-	/* Check if stack has less than two elements */
-	if (*head == NULL || (*head)->next == NULL)
+	stack_t *aux;
+	int sus, nodes;
+
+	aux = *head;
+	for (nodes = 0; aux != NULL; nodes++)
+		aux = aux->next;
+	if (nodes < 2)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
-		handle_error();
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
-
-	int result = (*head)->next->n - (*head)->n;
-	(*head)->next->n = result;
-
-	/* Remove the top element */
-	stack_t *temp = *head;
-	*head = (*head)->next;
-	(*head)->prev = NULL;
-	free(temp);
+	aux = *head;
+	sus = aux->next->n - aux->n;
+	aux->next->n = sus;
+	*head = aux->next;
+	free(aux);
 }
